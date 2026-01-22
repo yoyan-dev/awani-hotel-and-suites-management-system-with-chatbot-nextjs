3e7d0591ab4295061e909418178764ba5f445ecd
Yoyan <yoyan947@gmail.com>
2026-01-22 22:29:39 +0800

    feat: update header component to comment out Notification; add Supabase auth helpers for improved session management

--- app/housekeeping/guests/_components/header.tsx
+++ app/housekeeping/guests/_components/header.tsx
@@ -4,7 +4,7 @@ import Notification from "../../_components/notification";
 export default function Header() {
   return (
     <div>
-      <Notification />
+      {/* <Notification /> */}
       <div className="rounded mb-4 flex items-center justify-between">
         <div>
           <h1 className="text-2xl font-bold">Guests Movement</h1>
